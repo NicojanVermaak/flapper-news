fb30a50cd24ea122d4eea4720197f4c645a7f2cd
Nicojan Vermaak <nicojanvermaak@gmail.com>
2017-01-17 13:21:12 +0200

    Wiring Everything Up

--- flapper-news/public/javascripts/angularApp.js
+++ flapper-news/public/javascripts/angularApp.js
@@ -12,18 +12,28 @@ function($stateProvider, $urlRouterProvider) {
     .state('home', {
       url: '/home',
       templateUrl: '/home.html',
-      controller: 'MainCtrl'
+      controller: 'MainCtrl',
+      resolve: {
+        postPromise: ['posts', function(posts){
+          return posts.getAll();
+        }]
+      }
     })
     .state('posts', {
   url: '/posts/{id}',
   templateUrl: '/posts.html',
-  controller: 'PostsCtrl'
+  controller: 'PostsCtrl',
+  resolve: {
+    post: ['$stateParams','posts',function($stateParams,posts){
+      return posts.get($stateParams.id);
+    }]
+  }
 });
 
   $urlRouterProvider.otherwise('home');
 }]);
 
-app.factory('posts', [function(){
+app.factory('posts', ['$http',function($http){
   var o = {
     posts: [  {title: 'post 1', upvotes: 5},
       {title: 'post 2', upvotes: 2},
@@ -31,6 +41,44 @@ app.factory('posts', [function(){
       {title: 'post 4', upvotes: 9},
       {title: 'post 5', upvotes: 4}]
   };
+
+  o.getAll = function() {
+     return $http.get('/posts').success(function(data){
+       angular.copy(data, o.posts);
+     });
+   };
+
+   o.create = function(post) {
+     return $http.post('/posts', post).success(function(data){
+       o.posts.push(data);
+     });
+   };
+
+   o.upvote = function(post) {
+  return $http.put('/posts/' + post._id + '/upvote')
+    .success(function(data){
+      post.upvotes += 1;
+    });
+};
+
+o.get = function(id) {
+  return $http.get('/posts/' + id).then(function(res){
+    return res.data;
+  });
+};
+
+
+o.addComment = function(id, comment) {
+  return $http.post('/posts/' + id + '/comments', comment);
+};
+
+o.upvoteComment = function(post, comment) {
+  return $http.put('/posts/' + post._id + '/comments/'+ comment._id + '/upvote')
+    .success(function(data){
+      comment.upvotes += 1;
+    });
+};
+
   return o;
 }]);
 
@@ -43,41 +91,43 @@ function($scope,posts){
 
   $scope.test = 'Hello world!';
 
-  $scope.posts.push({
+$scope.addPost = function(){
+  if(!$scope.title || $scope.title === '') { return; }
+  posts.create({
     title: $scope.title,
     link: $scope.link,
-    upvotes: 0,
-    comments: [
-      {author: 'Joe', body: 'Cool post!', upvotes: 0},
-      {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
-    ]
   });
-
   $scope.title = '';
   $scope.link = '';
+};
 
 $scope.incrementUpvotes = function(post) {
-  post.upvotes += 1;
+  posts.upvote(post);
 };
+
 }]);
 
 
 app.controller('PostsCtrl', [
 '$scope',
-'$stateParams',
 'posts',
-function($scope, $stateParams, posts){
+'post',
+function($scope, posts, post){
 
-$scope.post = posts.posts[$stateParams.id];
+$scope.post = post
 
 $scope.addComment = function(){
   if($scope.body === '') { return; }
-  $scope.post.comments.push({
+  posts.addComment(post._id, {
     body: $scope.body,
     author: 'user',
-    upvotes: 0
+  }).success(function(comment) {
+    $scope.post.comments.push(comment);
   });
   $scope.body = '';
 };
 
+$scope.incrementUpvotes = function(comment){
+  posts.upvoteComment(post, comment);
+};
 }]);
--- flapper-news/routes/index.js
+++ flapper-news/routes/index.js
@@ -77,21 +77,22 @@ router.post('/posts/:post/comments', function(req, res, next) {
 });
 
 
-router.put('posts/:post/comments/:comment/upvote', function(req,res,next){
-  req.comment.upvote(function(err, post){
-    if (err) {return next(err);}
-    res.json(post);
+router.put('/posts/:post/comments/:comment/upvote', function(req, res, next) {
+  req.comment.upvote(function(err, comment){
+    if (err) { return next(err); }
+
+    res.json(comment);
   });
 });
 
-router.param('comment',function(req,res,next,id){
+router.param('comment', function(req, res, next, id) {
   var query = Comment.findById(id);
 
-  query.exec(function (err,comment){
-    if (err) {return next(err);}
-    if (!post) {return next(new Error('can\'t find comment'));}
+  query.exec(function (err, comment){
+    if (err) { return next(err); }
+    if (!comment) { return next(new Error("can't find comment")); }
 
-    req.comment=comment;
+    req.comment = comment;
     return next();
   });
 });
